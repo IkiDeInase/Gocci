56348bf359f470cbbdb4d3626907a0d05a7d4d50
fuji kinaga <kinakita071031@gmail.com>
2015-08-22 19:36:13 +0900

    Setting Notification Add

--- Gocci/app/src/main/java/com/inase/android/gocci/data/MyGcmListenerService.java
+++ Gocci/app/src/main/java/com/inase/android/gocci/data/MyGcmListenerService.java
@@ -17,11 +17,17 @@ import com.inase.android.gocci.Event.NotificationNumberEvent;
 import com.inase.android.gocci.R;
 import com.inase.android.gocci.common.SavedData;
 
+import java.util.Arrays;
+
 /**
  * Created by kinagafuji on 15/08/12.
  */
 public class MyGcmListenerService extends GcmListenerService {
     private static final String TAG = "MyGcmListenerService";
+
+    private static final String GOCHI_TEMPLATE = "さんからgochi!されました";
+    private static final String COMMENT_TEMPLATE = "さんからコメントされました";
+    private static final String FOLLOW_TEMPLATE = "さんからフォローされました";
 
     /**
      * Called when message is received.
@@ -35,19 +41,31 @@ public class MyGcmListenerService extends GcmListenerService {
     public void onMessageReceived(String from, Bundle data) {
         String def = data.getString("default");
         Log.e(TAG, "From: " + from);
+        Integer[] list = SavedData.getSettingNotifications(this);
+        if (def != null) {
+            if (def.matches(".*" + GOCHI_TEMPLATE + ".*")) {
+                if (Arrays.asList(list).contains(0)) {
+                    sendNotification(def);
+                }
+            } else if (def.matches(".*" + COMMENT_TEMPLATE + ".*")) {
+                if (Arrays.asList(list).contains(1)) {
+                    sendNotification(def);
+                }
+            } else if (def.matches(".*" + FOLLOW_TEMPLATE + ".*")) {
+                if (Arrays.asList(list).contains(2)) {
+                    sendNotification(def);
+                }
+            }
 
-        /**
-         * Production applications would usually process the message here.
-         * Eg: - Syncing with server.
-         *     - Store message in local database.
-         *     - Update UI.
-         */
-
-        /**
-         * In some cases it may be useful to show a notification indicating to the user
-         * that a message was received.
-         */
-        sendNotification(def);
+            if (!def.equals("投稿が完了しました！")) {
+                int badge_num = SavedData.getNotification(getApplicationContext());
+                BusHolder.get().post(new NotificationNumberEvent(badge_num + 1, def));
+                SavedData.setNotification(getApplicationContext(), badge_num + 1);
+            } else {
+                int badge_num = SavedData.getNotification(getApplicationContext());
+                BusHolder.get().post(new NotificationNumberEvent(badge_num, def));
+            }
+        }
     }
     // [END receive_message]
 
@@ -57,11 +75,6 @@ public class MyGcmListenerService extends GcmListenerService {
      * @param msg GCM message received.
      */
     private void sendNotification(String msg) {
-        if (!msg.equals("投稿が完了しました。")) {
-            int badge_num = SavedData.getNotification(getApplicationContext());
-            BusHolder.get().post(new NotificationNumberEvent(badge_num + 1, msg));
-            SavedData.setNotification(getApplicationContext(), badge_num + 1);
-
             Intent intent = new Intent(this, SplashActivity.class);
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
@@ -80,10 +93,5 @@ public class MyGcmListenerService extends GcmListenerService {
                     (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
             notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
-
-        } else {
-            int badge_num = SavedData.getNotification(getApplicationContext());
-            BusHolder.get().post(new NotificationNumberEvent(badge_num, msg));
-        }
     }
 }
