a5502b9607f25132fea38432ce2ea972570b32de
fuji kinaga <kinakita071031@gmail.com>
2015-08-25 15:56:29 +0900

    EditText focus bug fix

---
+++ Gocci/app/src/main/java/com/inase/android/gocci/Base/BlockEditTextDefaultFocusRelativeLayout.java
@@ -0,0 +1,18 @@
+package com.inase.android.gocci.Base;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by kinagafuji on 15/08/25.
+ */
+public class BlockEditTextDefaultFocusRelativeLayout extends RelativeLayout {
+
+    public BlockEditTextDefaultFocusRelativeLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setFocusable(true);
+        setFocusableInTouchMode(true);
+        requestFocus();
+    }
+}
